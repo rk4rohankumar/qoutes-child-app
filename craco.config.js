04801af2c0ff6d7cf98559ab629ec2d7c9b09a48
rohan <rwpmns@gmail.com>
2025-02-05 00:02:08 +0530

    added deployement link

--- craco.config.js
+++ craco.config.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      webpackConfig.output.publicPath = 'http://localhost:3009/';
+      webpackConfig.output.publicPath = 'https://qoutes-child-app.vercel.app/';
 
       webpackConfig.plugins.push(
         new ModuleFederationPlugin({
